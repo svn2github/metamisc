4d80e9ef92303600d9261b01aa5e6a5fc2ad0b6a
nightlordtw <nightlordtw@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-09-12 13:18:33 +0000

    fix bug for plot function ($vcov does not exist) add function to allow pooling effect size data
    git-svn-id: svn://r-forge.r-project.org/svnroot/metamisc@111 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/riley.r
+++ pkg/R/riley.r
@@ -1,15 +1,115 @@
-riley <- function(X, ...) UseMethod("riley")
+riley <- function(X, type="effect.size", optimization = "Nelder-Mead", control = list(), ...) UseMethod("riley")
 
-# Estimated parameters
-# mu1 : logit(sensitivity)
-# mu2 : logit(specificity)
-# psi1 : standard deviation of mu1
-# psi2 : standard deviation of mu2
-# rhoT : (logit(rho+1)/2) where rho is the correlation between mu1 and mu2
-riley.default <-
+riley.default <- function(X, type="effect.size", optimization = "Nelder-Mead", control = list(), ...)
+{
+    est <- NA    
+    if (type=="test.accuracy") { est <- rileyDA(X, optimization = optimization, control=control, ...) }
+    else { est <- rileyES(X, optimization = optimization, control=control, ...)}
+
+    class(est) <- "riley"
+    est
+}
+
+# effect sizes data meta-analysis
+rileyES <- function(X = NULL, Y1, Y2, vars1, vars2, optimization = "Nelder-Mead", control = list(),...)
+{
+	if(!is.null(X)){
+		X <- as.data.frame(X)
+		origdata <- X
+		Y1 <- X$Y1
+		Y2 <- X$Y2
+		vars1 <- X$vars1
+		vars2 <- X$vars2
+	} else {
+		origdata <- cbind(Y1,vars1,Y2,vars2)
+		colnames(origdata) = c("Y1","vars1","Y2","vars2")
+	}
+  
+	numstudies = length(Y1)
+	df <- numstudies - 5  
+	if(df < 0){warning("There are very few primary studies!")}
+	
+	vars = cbind(vars1, vars2)
+	Y = array(NA,dim=c((length(Y1)*2),1))
+	for (i in 1:length(Y1))
+	{
+		Y[((i-1)*2+1)] = Y1[i]
+		Y[((i-1)*2+2)] = Y2[i]
+	}
+	
+	#Calculate starting values for optim
+	pars.start = c(0,0,0,0,0)
+	if (numstudies >= 2) {
+		sumlY1 <- uvmeta(r=Y1, vars=vars1, method="MOM")
+		sumlY2 <- uvmeta(r=Y2, vars=vars2, method="MOM")
+		pars.start = c(sumlY1$ranef$mean,sumlY2$ranef$mean,abs(sqrt(sumlY1$ranef$var)),abs(sqrt(sumlY2$ranef$var)),0)
+	}
+	
+	negfullloglik <- function(pars,Y,vars)
+	{
+		beta1 = pars[1]
+		beta2 = pars[2]
+		psisq1 = pars[3]**2 #ensure variance is positive
+		psisq2 = pars[4]**2 #ensure variance is positive
+		rho = inv.logit(pars[5])*2-1 #ensure correlation is in [-1,1], and values in that interval move symmetric from -1 to 0 and from 1 to 0
+		k = 2 #2 endpoints
+		n = dim(Y)[1]/2
+	
+		#Beta vector
+		Beta = rbind(beta1,beta2)
+	
+		#Design matrix
+		X = array(NA,dim=c(n*2,2))
+		X[,1] = rep(c(1,0),n)
+		X[,2] = rep(c(0,1),n)
+	
+		#Create Phi matrix
+		Phi = array(0,dim=c((n*2),(n*2)))
+		for (i in 1:n) {
+			Phi[((i-1)*2+1),((i-1)*2+1)] = vars[i,1]+psisq1
+			Phi[((i-1)*2+2),((i-1)*2+2)] = vars[i,2]+psisq2
+			Phi[((i-1)*2+1),((i-1)*2+2)] = rho*sqrt((vars[i,1]+psisq1)*(vars[i,2]+psisq2))
+			Phi[((i-1)*2+2),((i-1)*2+1)] = rho*sqrt((vars[i,1]+psisq1)*(vars[i,2]+psisq2))
+		}
+		
+		#Minimize the negative of the restricted log-lkh
+		0.5*((n-k)*log(2*pi)-log(det(t(X)%*%X))+log(det(Phi))+log(det(t(X)%*%solve(Phi)%*%X))+(t(Y-X%*%Beta)%*%solve(Phi)%*%(Y-X%*%Beta)))
+	}
+	
+	fit = optim(pars.start,negfullloglik,Y=Y,vars=vars,method=optimization,hessian=T,control=control)
+	
+	if(fit$convergence != 0) { 
+		if(fit$convergence == 1) warning ("Iteration limit had been reached.")
+		else if (fit$convergence == 10) warning("Degeneracy of the Nelder-Mead simplex.")
+		else if (fit$convergence == 51 | fit$convergence == 52) warning(fit$message)
+	}
+	
+	beta1 = fit$par[1]
+	beta2 = fit$par[2]
+	psi1 = abs(fit$par[3])
+	psi2 = abs(fit$par[4])
+	rhoT = fit$par[5]
+	coefficients = c(beta1,beta2,psi1,psi2,rhoT)
+	names(coefficients) = c("beta1","beta2","psi1","psi2","rhoT")
+	
+	hessian = fit$hessian
+	colnames(hessian) = c("beta1","beta2","psi1","psi2","rhoT")
+	rownames(hessian) = c("beta1","beta2","psi1","psi2","rhoT")
+	
+	if (length(which(eigen(fit$hessian,symmetric=TRUE)$values<0))>0) warning("The Hessian contains negative eigenvalues!")
+	
+	iterations <- fit$iterations
+	logLik <- -fit$value
+	
+	output <- list(coefficients = coefficients, hessian = hessian, df = df, nobs = 2*numstudies, logLik = logLik,
+			   iterations = (iterations+1), call = match.call(), data = origdata, type="effect.size")  
+	return(output)
+}
+
+# Diagnostic test accuracy data meta-analysis
+rileyDA <-
   function(X = NULL, TP, FN, FP, TN, correction = 0.5, 
-           correction.control = "all", optimization = "Nelder-Mead", 
-		   control = list(), ...)
+           correction.control = "all", optimization = "Nelder-Mead", control = list(), ...)
   {
       if(!is.null(X)){
         X <- as.data.frame(X)
@@ -33,11 +133,6 @@ riley.default <-
         TN <- correction + TN
       }
       
-      # Numstudies
-      numstudies = length(TP)
-      df <- numstudies - 5  
-      if(df < 0){warning("There are very few primary studies!")}
-     
       #Calculate sensitivities and specificities (original scale)
       number.of.pos <- TP + FN
       number.of.neg <- FP + TN
@@ -51,84 +146,10 @@ riley.default <-
       var.logit.sens <- 1/(sens*(1-sens)*number.of.pos)
       var.logit.fpr <- 1/(fpr*(1-fpr)*number.of.neg)
       
-      vars = cbind(var.logit.sens,var.logit.fpr)
+      output = rileyES(Y1=logit.sens,Y2=logit.fpr,vars1=var.logit.sens,vars2=var.logit.fpr,optimization = optimization, control = control, ...)
+      output$type = "test.accuracy"
+      output$data = origdata
       
-      Y = array(NA,dim=c((length(logit.sens)*2),1))
-      for (i in 1:length(logit.sens))
-      {
-        Y[((i-1)*2+1)] = logit.sens[i]
-        Y[((i-1)*2+2)] = logit.fpr[i]
-      }
-
-      #Calculate starting values for optim
-	  pars.start = c(0,0,0,0,0)
-	  if (numstudies >= 2) {
-		sumlsens <- uvmeta(r=logit.sens, vars=var.logit.sens, method="MOM")
-		sumlfpr  <- uvmeta(r=logit.fpr, vars=var.logit.fpr, method="MOM")
-		pars.start = c(sumlsens$ranef$mean,sumlfpr$ranef$mean,abs(sqrt(sumlsens$ranef$var)),abs(sqrt(sumlsens$ranef$var)),0)
-	  }
-
-      negfullloglik <- function(pars,Y,vars)
-      {
-        beta1 = pars[1]
-        beta2 = pars[2]
-        psisq1 = pars[3]**2 #ensure variance is positive
-        psisq2 = pars[4]**2 #ensure variance is positive
-        rho = inv.logit(pars[5])*2-1 #ensure correlation is in [-1,1], and values in that interval move symmetric from -1 to 0 and from 1 to 0
-        k = 2 #2 endpoints
-        n = dim(Y)[1]/2
-        
-        #Beta vector
-        Beta = rbind(beta1,beta2)
-        
-        #Design matrix
-        X = array(NA,dim=c(n*2,2))
-        X[,1] = rep(c(1,0),n)
-        X[,2] = rep(c(0,1),n)
-        
-        #Create Phi matrix
-        Phi = array(0,dim=c((n*2),(n*2)))
-        for (i in 1:n) {
-          Phi[((i-1)*2+1),((i-1)*2+1)] = vars[i,1]+psisq1
-          Phi[((i-1)*2+2),((i-1)*2+2)] = vars[i,2]+psisq2
-          Phi[((i-1)*2+1),((i-1)*2+2)] = rho*sqrt((vars[i,1]+psisq1)*(vars[i,2]+psisq2))
-          Phi[((i-1)*2+2),((i-1)*2+1)] = rho*sqrt((vars[i,1]+psisq1)*(vars[i,2]+psisq2))
-        }
-        
-        #Minimize the negative of the restricted log-lkh
-        0.5*((n-k)*log(2*pi)-log(det(t(X)%*%X))+log(det(Phi))+log(det(t(X)%*%solve(Phi)%*%X))+(t(Y-X%*%Beta)%*%solve(Phi)%*%(Y-X%*%Beta)))
-      }
-      
-      fit = optim(pars.start,negfullloglik,Y=Y,vars=vars,method=optimization,hessian=T,control=control)
-	  
-	  if(fit$convergence != 0) { 
-		if(fit$convergence == 1) warning ("Iteration limit had been reached.")
-		else if (fit$convergence == 10) warning("Degeneracy of the Nelder-Mead simplex.")
-		else if (fit$convergence == 51 | fit$convergence == 52) warning(fit$message)
-	  }
-      
-      mu1 = fit$par[1]
-      mu2 = fit$par[2]
-      psi1 = abs(fit$par[3])
-      psi2 = abs(fit$par[4])
-      #rho = inv.logit(fit$par[5])*2-1
-      rhoT = fit$par[5]
-      
-      coefficients = c(mu1,mu2,psi1,psi2,rhoT)
-      names(coefficients) = c("mu1","mu2","psi1","psi2","rhoT")
-      
-      hessian = fit$hessian
-      colnames(hessian) = c("mu1","mu2","psi1","psi2","rhoT")
-      rownames(hessian) = c("mu1","mu2","psi1","psi2","rhoT")
-	  
-	  if (length(which(eigen(fit$hessian,symmetric=TRUE)$values<0))>0) warning("The Hessian contains negative eigenvalues!")
-      
-      iterations <- fit$iterations
-      logLik <- -fit$value
-      
-      output <- list(coefficients = coefficients, hessian = hessian, df = df, nobs = 2*numstudies, logLik = logLik,
-                   iterations = (iterations+1), call = match.call(), data = origdata)
-      class(output) <- "riley"
       return(output)
   }
 
@@ -146,18 +167,25 @@ predict.riley <- function(object, level = 0.95, ...)
 {
   alpha = (1-level)/2
   
-  #Prediction interval
   predint=array(NA,dim=c(2,3))
-  rownames(predint) = c("Sens","FPR")
   colnames(predint) = c("Estimate", paste((alpha*100),"%"),paste(((1-alpha)*100),"%"))
   df = object$df
-  if (df > 0)
+  
+  if (object$type=="test.accuracy")
   {
-    predint[1,] = inv.logit(c(coefficients(object)["mu1"],(qt(alpha,df=df)*sqrt((coefficients(object)["psi1"]**2)+diag(vcov(object))[1])+coefficients(object)["mu1"]),(qt((1-alpha),df=df)*sqrt((coefficients(object)["psi1"]**2)+diag(vcov(object))[1])+coefficients(object)["mu1"])))
-    predint[2,] = inv.logit(c(coefficients(object)["mu2"],(qt(alpha,df=df)*sqrt((coefficients(object)["psi2"]**2)+diag(vcov(object))[2])+coefficients(object)["mu2"]),(qt((1-alpha),df=df)*sqrt((coefficients(object)["psi2"]**2)+diag(vcov(object))[2])+coefficients(object)["mu2"])))
+	rownames(predint) = c("Sens","FPR")
+	if (df > 0)
+	{
+		predint[1,] = inv.logit(c(coefficients(object)["beta1"],(qt(alpha,df=df)*sqrt((coefficients(object)["psi1"]**2)+diag(vcov(object))[1])+coefficients(object)["beta1"]),(qt((1-alpha),df=df)*sqrt((coefficients(object)["psi1"]**2)+diag(vcov(object))[1])+coefficients(object)["beta1"])))
+		predint[2,] = inv.logit(c(coefficients(object)["beta2"],(qt(alpha,df=df)*sqrt((coefficients(object)["psi2"]**2)+diag(vcov(object))[2])+coefficients(object)["beta2"]),(qt((1-alpha),df=df)*sqrt((coefficients(object)["psi2"]**2)+diag(vcov(object))[2])+coefficients(object)["beta2"])))
+	} else {
+		predint[1,] = c(inv.logit(coefficients(object)["beta1"]),0,1)
+		predint[2,] = c(inv.logit(coefficients(object)["beta2"]),0,1)
+	}
   } else {
-    predint[1,] = c(inv.logit(coefficients(object)["mu1"]),0,1)
-    predint[2,] = c(inv.logit(coefficients(object)["mu2"]),0,1)
+	rownames(predint) = c("beta1","beta2")
+	predint[1,] = c(coefficients(object)["beta1"],(qt(alpha,df=df)*sqrt((coefficients(object)["psi1"]**2)+diag(vcov(object))[1])+coefficients(object)["beta1"]),(qt((1-alpha),df=df)*sqrt((coefficients(object)["psi1"]**2)+diag(vcov(object))[1])+coefficients(object)["beta1"]))
+	predint[2,] = c(coefficients(object)["beta2"],(qt(alpha,df=df)*sqrt((coefficients(object)["psi2"]**2)+diag(vcov(object))[2])+coefficients(object)["beta2"]),(qt((1-alpha),df=df)*sqrt((coefficients(object)["psi2"]**2)+diag(vcov(object))[2])+coefficients(object)["beta2"]))
   }
   predint
 }
@@ -165,18 +193,21 @@ predict.riley <- function(object, level = 0.95, ...)
 
 summary.riley <- function(object, level = 0.95, ...)
 {
-  confints <- cbind(object$coefficients, confint(object,level=level))
-  colnames(confints)[1] <- "Estimate"
-  confints <- rbind(inv.logit(confints[1:2,]),confints)
-  rownames(confints)[1:2] <-  c("Sens", "FPR") 
-  
-  #Transform last parameter back to rho
-  confints[7,] =  inv.logit(confints[7,])*2-1
-  rownames(confints)[7] = "rho"
-  
-  res <- list(call=object$call, confints = confints)
-  class(res) <- "summary.riley"
-  res
+	confints <- cbind(object$coefficients, confint(object,level=level))
+	colnames(confints)[1] <- "Estimate"
+	
+	if (object$type=="test.accuracy") {
+		confints <- rbind(inv.logit(confints[1:2,]),confints)
+		rownames(confints)[1:2] <-  c("Sens", "FPR") 
+	} 
+	
+	#Transform last parameter back to rho
+	confints["rhoT",] =  inv.logit(confints["rhoT",])*2-1
+	rownames(confints)[which(rownames(confints)=="rhoT")] = "rho"
+	
+	res <- list(call=object$call, confints = confints)
+	class(res) <- "summary.riley"
+	res
 }
 
 print.summary.riley <- function(x, ...)
@@ -199,26 +230,45 @@ vcov.riley <- function(object, ...){
 logLik.riley <- function(object, ...){object$logLik}
 
 
-plot.riley <- function(x, plotsumm = TRUE, plotnumerics = TRUE, level = 0.95, main="",
+plot.riley <- function(object, plotsumm = TRUE, plotnumerics = TRUE, level = 0.95, main="",
                        ylim = c(0,1), xlim = c(0,1), pch = 1, lty = 1, lwd = 1, cex.numerics=0.45,
                        add=F, ...)
 {
-
-  if (!add) plot(-500,-500, type = "l", xlim = xlim, ylim = ylim, xlab="1-Specificity",ylab="Sensitivity",main=main)
-  
-  alpha = (1-level)/2
-  FP <- x$data$FP
-  negatives <- FP + x$data$TN
-  FPR <- FP/negatives
-  mu = x$coefficients[c("mu1","mu2")]
-  Sigma = x$vcov[c(1,2),c(1,2)] 
-  mu.ellipse <- ellipse(Sigma, centre = mu, level = level)           
-  polygon(inv.logit(mu.ellipse[,2]),inv.logit(mu.ellipse[,1]),lty=lty, lwd=lwd)
-  
-  if(plotsumm) points(inv.logit(mu[2]),inv.logit(mu[1]),pch=pch) # add the point estimate of the mean
-  
-  if(plotnumerics) {
-    ci = summary(x,level=level)[2]$confints
+	alpha = (1-level)/2
+	
+	if (object$type=="test.accuracy") {
+		xlab = "1-Specificity"
+		ylab = "Sensitivity"
+		
+		FP <- object$data$FP
+		negatives <- FP + object$data$TN
+		FPR <- FP/negatives
+		mu = object$coefficients[c("beta2","beta1")]
+		Sigma = vcov(object)[c("beta2","beta1"),c("beta2","beta1")] 
+		mu.ellipse <- ellipse(Sigma, centre = mu, level = level) 
+		summary1 = inv.logit(mu[1])
+		summary2 = inv.logit(mu[2])
+		ellipse1 = inv.logit(mu.ellipse[,1])
+		ellipse2 = inv.logit(mu.ellipse[,2])
+	} else {
+		plotnumerics = FALSE
+		xlab = "Y1"
+		ylab = "Y2"
+		mu = object$coefficients[c("beta1","beta2")]
+		Sigma = vcov(object)[c(1,2),c(1,2)] 
+		mu.ellipse <- ellipse(Sigma, centre = mu, level = level) 
+		summary1 = mu[1]
+		summary2 = mu[2]
+		ellipse1 = mu.ellipse[,1]
+		ellipse2 = mu.ellipse[,2]
+	}
+	
+	if (!add) plot(-500,-500, type = "l", xlim = xlim, ylim = ylim, xlab=xlab,ylab=ylab,main=main)
+	polygon(ellipse1,ellipse2,lty=lty, lwd=lwd)
+	if(plotsumm) points(summary1,summary2,pch=pch) # add the point estimate of the mean
+	
+	if(plotnumerics) {
+    ci = summary(object,level=level)[2]$confints
     text(0.8,0.15,labels="Estimate",pos=2,cex=cex.numerics)
     text(0.9,0.15,labels=paste((alpha*100),"% CI",sep=""),pos=2,cex=cex.numerics)
     text(1.0,0.15,labels=paste(((1-alpha)*100),"% CI",sep=""),pos=2,cex=cex.numerics)
