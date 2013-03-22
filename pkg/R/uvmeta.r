70c3020ec2b39831000ed1819570c7ad126fe50d
nightlordtw <nightlordtw@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-03-22 11:29:16 +0000

    improve uvmeta to allow for NAs
    git-svn-id: svn://r-forge.r-project.org/svnroot/metamisc@192 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/uvmeta.r
+++ pkg/R/uvmeta.r
@@ -3,55 +3,68 @@
 # fixed-effects and random-effects meta-analysis.
 # r    : Vector of the effect sizes
 # vars : Vector of the effect variances
-################################################################################
+###############################################################################
 # Author  : Thomas Debray
 # Version : 10 May 2011
-################################################################################
+###############################################################################
 # Example
 # example.r = c(0.10,0.30,0.35,0.65,0.45,0.15)
 # example.var = c(0.03,0.03,0.05,0.01,0.05,0.02)
-# macc(example.r,example.var)
-################################################################################
-uvmeta <- function(r, vars, model="random", method="MOM", pars=list(quantiles = c(0.025, 0.25, 0.5, 0.75, 0.975), n.chains=4, n.adapt=5000, n.init=1000, n.iter=10000), verbose=FALSE, ...) UseMethod("uvmeta")
+# uvmeta(example.r,example.var)
+###############################################################################
+uvmeta <- function(r, vars, model="random", method="MOM", na.action,
+                   pars=list(quantiles=c(0.025, 0.25, 0.5, 0.75, 0.975), 
+                             n.chains=4, n.adapt=5000, n.init=1000, 
+                             n.iter=10000), verbose=FALSE, ...) 
+  UseMethod("uvmeta")
 
-uvmeta.default <- function(r,vars, model="random", method="MOM", pars=list(quantiles = c(0.025, 0.25, 0.5, 0.75, 0.975), n.chains=4, n.adapt=5000, n.init=1000, n.iter=10000), verbose=FALSE, ...)
+uvmeta.default <- function(r,vars, model="random", method="MOM", na.action, 
+                           pars=list(quantiles = c(0.025, 0.25, 0.5, 0.75, 0.975),
+                                     n.chains=4, n.adapt=5000, n.init=1000, 
+                                     n.iter=10000), verbose=FALSE, ...)
 {
-    x <- as.vector(r)
-    y <- as.vector(vars)
-    est <- NA    
-    if (length(x)!=length(y)) {warning("The vectors 'r' and 'vars' have a different size!")}
-    if (method == "MOM") { est <- uvmetaMOM(x, y, model, pars, verbose) }
-    else if (method == "bayes") { est <- uvmetaBayes(x,y, model, pars, verbose) }
 
-    est$call <- match.call()
-    class(est) <- "uvmeta"
-    est
-}
-
-uvmetaMOM <- function(r,vars, model="random", pars=list(quantiles = c(0.025, 0.25, 0.5, 0.75, 0.975)),verbose=FALSE) {
-    # Degrees of freedom
-    numstudies = length(r)
-    dfr = numstudies-1
-    
-    if(numstudies < 3){warning("There are very few primary studies!")}
-
+  ds <- cbind(as.vector(r),as.vector(vars))
+  est <- NA    
+  
+  if (length(x)!=length(y)) {
+    stop("The vectors 'r' and 'vars' have a different length!")
+  }
+  
+  if (missing(na.action)) 
+    na.action <- "na.fail"
+  if (length(na.action)) 
+    ds <- do.call(na.action, list(ds))
+  
+  
+  #############################################################################
+  # Start analyses
+  #############################################################################
+  numstudies = dim(ds)[1]
+  dfr = numstudies-1
+  
+  if(numstudies < 3) {
+    warning("There are very few primary studies!")
+  }
+  
+  if (method == "MOM") { 
     ############################################################################
     # FIXED EFFECTS MODEL
     ############################################################################
     w = 1/vars
-
+    
     #Combined effect
     weighted_Tbar = sum(r*w)/sum(w)
-
+    
     # Variance of the combined effect
     var_T = 1/sum(w)
-
+    
     # Standard error combined effect
     se_T = sqrt(var_T)
-
+    
     # The Z-value
     z_T = weighted_Tbar/se_T
-
+    
     ############################################################################
     # RANDOM EFFECTS MODEL
     ############################################################################
@@ -66,36 +79,36 @@ uvmetaMOM <- function(r,vars, model="random", pars=list(quantiles = c(0.025, 0.2
     # ratio (Birge, 1932)
     H_sq = 1    #H2
     se_lnH = 0  #SE(ln(H2))
-
+    
     # Between-study variance
     if (Q > dfr) {
-        re_C =  sum(w) - sum(w**2)/sum(w)
-        between_study_var = (Q - dfr)/re_C
-        I_sq = (Q-dfr)/Q
-        H_sq = Q/dfr
-        se_lnH = (log(Q)-log(dfr))/(2*(sqrt(2*Q)-sqrt((2*length(r))-3)))
+      re_C =  sum(w) - sum(w**2)/sum(w)
+      between_study_var = (Q - dfr)/re_C
+      I_sq = (Q-dfr)/Q
+      H_sq = Q/dfr
+      se_lnH = (log(Q)-log(dfr))/(2*(sqrt(2*Q)-sqrt((2*length(r))-3)))
     } else {
-        between_study_var = 0
-        se_lnH = sqrt((1/(2*(length(r)-2)))*(1-(1/(3*((length(r)-2)**2)))))
+      between_study_var = 0
+      se_lnH = sqrt((1/(2*(length(r)-2)))*(1-(1/(3*((length(r)-2)**2)))))
     }
     #varQ = 2*dfr + 4*(sum(w)-sum(w**2)/sum(w))*between_study_var + 2*(sum(w**2)-2*((sum(w**3)/sum(w))+(sum(w**2)**2)/sum(w)**2))*between_study_var**2
     #varTauSq = varQ/(sum(w)-sum(w**2)/sum(w))**2
-
+    
     # Within-study plus between-study variance
     re_v = vars + between_study_var
-
+    
     # Updated weights
     re_w = 1/re_v
-
+    
     # Combined effect
     re_weighted_Tbar =  sum(r*re_w)/sum(re_w)
-
+    
     # Variance of the combined effect
     re_var_T  = 1/sum(re_w)
-
+    
     # Standard error of combined effect
     re_se_T = sqrt(re_var_T)
-
+    
     # The Z-value
     re_z_T = re_weighted_Tbar/re_se_T
     
@@ -115,60 +128,57 @@ uvmetaMOM <- function(r,vars, model="random", pars=list(quantiles = c(0.025, 0.2
     #Q.critical = qchisq(0.95,df=(length(r)-1))
     Q_p = 1-pchisq(Q,df=(length(r)-1))
     Q.results = list(Q=Q,p.value=Q_p)
-
+    
     results = array(NA,dim=c(4, length(pars$quantiles)+2))
     colnames(results) = c("Mean","Var",paste(pars$quantiles*100,"%",sep=""))
     rownames(results) = c("mu","tausq","Q","Isq")
     results[3,] = c(Q,NA,qchisq(pars$quantiles,df=dfr))
     
     if (model=="random") {
-        results[1,] = c(re_weighted_Tbar,re_var_T,re_weighted_Tbar+qnorm(pars$quantiles)*sqrt(re_var_T))
-	results[2,] = c(between_study_var,NA,rep(NA,length(pars$quantiles)))
-	results[4,] = c(I_sq,NA,rep(NA,length(pars$quantiles)))
+      results[1,] = c(re_weighted_Tbar,re_var_T,re_weighted_Tbar+qnorm(pars$quantiles)*sqrt(re_var_T))
+      results[2,] = c(between_study_var,NA,rep(NA,length(pars$quantiles)))
+      results[4,] = c(I_sq,NA,rep(NA,length(pars$quantiles)))
     } else if (model=="fixed") {
-        results[1,] = c(weighted_Tbar,var_T,weighted_Tbar+qnorm(pars$quantiles)*sqrt(var_T))
-	results[2,] = c(0,0,rep(NA,length(pars$quantiles)))
+      results[1,] = c(weighted_Tbar,var_T,weighted_Tbar+qnorm(pars$quantiles)*sqrt(var_T))
+      results[2,] = c(0,0,rep(NA,length(pars$quantiles)))
     }
- 
-    ############################################################################
-    # Output
-    ############################################################################
-    out <- list(results=results,model=model,df=dfr,numstudies=numstudies)
-    class(out) <- "uvmeta"
-    return (out)
-}
-
-uvmetaBayes <- function(r,vars, model="random",pars=list(quantiles = c(0.025, 0.25, 0.5, 0.75, 0.975), n.chains=4, n.adapt=5000, n.init=1000, n.iter=10000),verbose=FALSE) {
-	numstudies = length(r)	
-	dfr = numstudies-1
+    est <- list(results=results,model=model,df=dfr,numstudies=numstudies)
+  } else if (method == "bayes") { 
     quiet = !verbose
+    
+    modelfile <-  if (model=="random") system.file(package="metamisc", "model", "uvmeta_ranef.bug") else system.file(package="metamisc", "model", "uvmeta_fixef.bug")
+    jags <- jags.model(modelfile,
+                       data = list('r' = r,
+                                   'vars' = vars,
+                                   'k' = numstudies), #prior precision matrix
+                       n.chains = pars$n.chains,
+                       n.adapt = pars$n.adapt,
+                       quiet = quiet)
+    update(jags, pars$n.init) #initialize
+    samples <- coda.samples(jags, c('mu','tausq','Q','Isq'),n.iter=pars$n.iter)
+    
+    results <- summary(samples,quantiles=pars$quantiles) #summary.mcmc(samples,quantiles=pars$quantiles)
+    
+    results.overview = array(NA,dim=c(dim(results[[1]])[1], length(pars$quantiles)+2))
+    colnames(results.overview) = c("Mean","Var",paste(pars$quantiles*100,"%",sep=""))
+    rownames(results.overview) = rownames(results[[2]])
+    results.overview[,1] = (results[[1]])[,"Mean"]
+    results.overview[,2] = (results[[1]])[,"SD"]**2
+    for (i in 1:length(pars$quantiles)) {
+      results.overview[,(i+2)] = (results[[2]])[,i]
+    }
+    
+    est <- list(results=results.overview,model=model,df=dfr,numstudies=numstudies)
+  } else {
+    stop("Invalid meta-analysis method!")
+  }
+  
 
-	modelfile <-  if (model=="random") system.file(package="metamisc", "model", "uvmeta_ranef.bug") else system.file(package="metamisc", "model", "uvmeta_fixef.bug")
-	jags <- jags.model(modelfile,
-                     data = list('r' = r,
-                                 'vars' = vars,
-                                 'k' = numstudies), #prior precision matrix
-                     n.chains = pars$n.chains,
-                     n.adapt = pars$n.adapt,
-                     quiet = quiet)
-	update(jags, pars$n.init) #initialize
-	samples <- coda.samples(jags, c('mu','tausq','Q','Isq'),n.iter=pars$n.iter)
-
-	results <- summary(samples,quantiles=pars$quantiles) #summary.mcmc(samples,quantiles=pars$quantiles)
-
-	results.overview = array(NA,dim=c(dim(results[[1]])[1], length(pars$quantiles)+2))
-	colnames(results.overview) = c("Mean","Var",paste(pars$quantiles*100,"%",sep=""))
-	rownames(results.overview) = rownames(results[[2]])
-	results.overview[,1] = (results[[1]])[,"Mean"]
-	results.overview[,2] = (results[[1]])[,"SD"]**2
-	for (i in 1:length(pars$quantiles)) {
-		results.overview[,(i+2)] = (results[[2]])[,i]
-	}
-	
-	out <- list(results=results.overview,model=model,df=dfr,numstudies=numstudies)
-    	class(out) <- "uvmeta"
-    	return (out)
+    est$call <- match.call()
+    class(est) <- "uvmeta"
+    return(est)
 }
+
 
 
 print.uvmeta <- function(x, ...)
