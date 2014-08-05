723d1e0e966c3c4415de434e806aec5d354d7614
nightlordtw <nightlordtw@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-08-05 15:39:49 +0000

    simplify requirements calibration functions
    git-svn-id: svn://r-forge.r-project.org/svnroot/metamisc@240 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/basics.r
+++ pkg/R/basics.r
@@ -5,18 +5,18 @@ inv.logit <- function(x) {  if(is.numeric(x)) 1/(1+exp(-x)) else stop("x is not 
 
 validate <- function(x, ds.ipd, time.calibration=NA) {
   
-  cal.intercept <- function(x, y, lp) {
+  cal.intercept <- function(y, lp, family) {
     predictions <- as.data.frame(cbind(y,lp))
-    m.ll <- glm("y~1", offset=lp, data=predictions, family=x$family)
+    m.ll <- glm("y~1", offset=lp, data=predictions, family=family)
     results = array(NA, dim=2)
     names(results) = c("estimate", "se")
     results["estimate"] <- coefficients(m.ll)[1]
     results["se"] <- sqrt(vcov(m.ll)[1,1])
     return(results)
   }
-  cal.slope <- function(x, y, lp) {
+  cal.slope <- function(y, lp, family) {
     predictions <- as.data.frame(cbind(y,lp))
-    m.ll = glm(as.formula("y~lp"), data=predictions, family=x$family)
+    m.ll = glm(as.formula("y~lp"), data=predictions, family=family)
     results = array(NA, dim=2)
     names(results) = c("estimate", "se")
     results["estimate"] <- coefficients(m.ll)[2]
@@ -31,12 +31,13 @@ validate <- function(x, ds.ipd, time.calibration=NA) {
     yhat <- as.numeric(predict(x, newdata=ds.ipd, type="response"))
     outcome <- all.vars(formula(x))[1]
     y <- ds.ipd[,outcome]
+    
+    # Model discrimination
     roc.rule = roc(response=y, predictor=lp)
     
     num.observed <- sum(y)
     num.expected <- sum(yhat)
-    
-    p.observed <- sum(y)/length(y)
+    p.observed <- num.observed/length(y)
     
     # num events
     events.results <- c(num.observed, num.expected)
@@ -44,13 +45,13 @@ validate <- function(x, ds.ipd, time.calibration=NA) {
     
     # O/E ratio and standard error
     OE <- num.observed/num.expected
-    se.lnOE <- sqrt((1-p.observed)/sum(y))
+    se.lnOE <- sqrt((1-p.observed)/num.observed)
     OEresults <- c(OE, exp(log(OE)+qnorm(0.025)*se.lnOE), exp(log(OE)+qnorm(0.975)*se.lnOE))
     names(OEresults) <- c("O:E", "2.5%CI", "97.5%CI")
     
     #calibration slope and intercept
-    m.intercept <- cal.intercept(x, y, lp)
-    m.slope <- cal.slope(x, y, lp)
+    m.intercept <- cal.intercept(y, lp, x$family)
+    m.slope <- cal.slope(y, lp, x$family)
     
     predictions = as.data.frame(cbind(lp,yhat,y))
     
