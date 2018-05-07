6714777222c2c255d4b208786587d4ca9499a9e5
nightlordtw <nightlordtw@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2018-05-07 19:31:16 +0000

    added new use cases for restoration of SE(logOE)
    git-svn-id: svn://r-forge.r-project.org/svnroot/metamisc@435 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/tests/testthat/test_oecalc.R
+++ pkg/tests/testthat/test_oecalc.R
@@ -9,14 +9,25 @@ test_that("Standard error of log O:E ratio", {
   
   # Poisson distribution for total number of observed events
   logoese1 <- sqrt(1/EuroSCORE$n.events - 1/EuroSCORE$n)
-  logoese2 <- (oecalc(O=n.events, E=e.events, N=n, data=EuroSCORE, g="log(OE)"))$theta.se
-  expect_equal(logoese1, logoese2)
+  logoese2 <- sqrt((resoe.O.E.N(O=EuroSCORE$n.events, E=EuroSCORE$e.events, N=EuroSCORE$n, correction = 1/2, g="log(OE)"))[,2])
+  logoese3 <- (oecalc(O=n.events, E=e.events, N=n, data=EuroSCORE, g="log(OE)"))$theta.se
+  expect_equal(logoese1, logoese2, logoese3)
   
   # Binomial distribution for total number of observed events
-  logoese3 <- sqrt(1/EuroSCORE$n.events)
-  logoese4 <- sqrt((resoe.O.E(O=EuroSCORE$n.events, E=EuroSCORE$e.events, correction=1/2, g="log(OE)"))[,2])
-  logoese5 <- (oecalc(O=EuroSCORE$n.events, E=EuroSCORE$e.events, g="log(OE)"))$theta.se
-  expect_equal(logoese3, logoese4, logoese5)
+  logoese4 <- sqrt(1/EuroSCORE$n.events)
+  logoese5 <- sqrt((resoe.O.E(O=EuroSCORE$n.events, E=EuroSCORE$e.events, correction=1/2, g="log(OE)"))[,2])
+  logoese6 <- (oecalc(O=EuroSCORE$n.events, E=EuroSCORE$e.events, g="log(OE)"))$theta.se
+  expect_equal(logoese4, logoese5, logoese6)
+  
+  # Mixture of Poisson and Binomial
+  n.total <- EuroSCORE$n
+  n.total[1:10] <- NA
+  logoese7 <- sqrt(1/EuroSCORE$n.events - 1/EuroSCORE$n)
+  logoese7[1:10] <- sqrt(1/EuroSCORE$n.events)[1:10]
+  logoese8 <- sqrt((resoe.O.E.N(O=EuroSCORE$n.events, E=EuroSCORE$e.events, N=EuroSCORE$n, correction = 1/2, g="log(OE)"))[,2])
+  logoese8[1:10] <- sqrt((resoe.O.E(O=EuroSCORE$n.events, E=EuroSCORE$e.events, correction=1/2, g="log(OE)"))[,2])[1:10]
+  logoese9 <- (oecalc(O=EuroSCORE$n.events, E=EuroSCORE$e.events, n=n.total, g="log(OE)"))$theta.se
+  expect_equal(logoese7, logoese8, logoese8)
   
   # Derive from the 95% confidence interval
   OE <- EuroSCORE$n.events / EuroSCORE$e.events
